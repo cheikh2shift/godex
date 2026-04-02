cadab342ae265a85b5397454f3bacd1ee4e435c3
Cheikh Seck <cheikh@simsol.in>
2026-04-02 19:36:08 +0000

    display thinking text on 0 tool call

--- cmd/godex/main.go
+++ cmd/godex/main.go
@@ -854,7 +854,7 @@ promptLoop:
 			}
 			if !isToolCallResponse || len(toolCalls) == 0 {
 				// No valid tool calls - print thinking text and final response, then stop
-				if !printedStreamed && hasFinal {
+				if !printedStreamed {
 					thinkingText := extractThinkingText(preResp)
 					if thinkingText != "" {
 						fmt.Println(renderThinking(thinkingText))
