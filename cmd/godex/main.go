46f69e353eb7901d07ce083f0a5eeeb394ca96fc
Cheikh Seck <cheikh@simsol.in>
2026-04-04 12:03:32 +0000

    reduce tool result sent back

--- cmd/godex/main.go
+++ cmd/godex/main.go
@@ -967,7 +967,7 @@ promptLoop:
 						toolResults = append(toolResults, errMsg)
 						hasError = true
 					} else {
-						toolResults = append(toolResults, truncate(result, 5000))
+						toolResults = append(toolResults, truncate(result, 2500))
 					}
 				}
 				toolExecError = hasError
@@ -1952,7 +1952,7 @@ func runToolLoop(ctx context.Context, provider *config.Provider, servers []MCPSe
 					toolResults = append(toolResults, errMsg)
 					hasError = true
 				} else {
-					toolResults = append(toolResults, truncate(result, 5000))
+					toolResults = append(toolResults, truncate(result, 2500))
 				}
 			}
 			toolExecError = hasError
@@ -2327,7 +2327,7 @@ func executeToolCallsInParallel(ctx context.Context, servers []MCPServer, toolCa
 			results[result.index] = fmt.Sprintf("ERROR: %v", result.err)
 			hasError = true
 		} else {
-			results[result.index] = truncate(result.result, 5000)
+			results[result.index] = truncate(result.result, 2500)
 		}
 	}
 
