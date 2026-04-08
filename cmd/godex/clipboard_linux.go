f10e76ebe630550b242d0c2025641faa7ee26434
Cheikh Seck <cheikh@simsol.in>
2026-04-08 21:52:23 +0000

    Enable clipboard on Linux with X11, add WebSocket support to OpenAI provider

--- cmd/godex/clipboard_linux.go
+++ cmd/godex/clipboard_linux.go
@@ -17,9 +17,6 @@ func initClipboard() {
 
 func readClipboardText() []byte {
 	data := clipboard.Read(clipboard.FmtText)
-	if len(data) > 0 {
-		log.Printf("[Clipboard] read %d bytes", len(data))
-	}
 	return data
 }
 
